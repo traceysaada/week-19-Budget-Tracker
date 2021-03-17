6d12dff2312c7ac2dbd5b4ee82f3d2d9381f8443
Tracey Saada <tracey.saada@outlook.com>
2021-03-17 18:32:47 +0000

    added urls for cache area & urls that need cache

---
+++ Develop/public/service-worker.js
@@ -0,0 +1,15 @@
+//create variables that name the cache
+var CACHE = "budget-cache";
+var DATA_CACHE = "budget-data-cache";
+
+//create an array to store the urls i want to save in cache
+
+var applicationUrls = [
+    "/",
+    "/db.js",
+    "/index.js",
+    "/styles.css",
+    "/manifest.json",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+]
