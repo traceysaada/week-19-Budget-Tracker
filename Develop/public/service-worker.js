a3bae128c46a9214bbc397496a84667f6120d2cf
Tracey Saada <tracey.saada@outlook.com>
2021-03-17 18:41:35 +0000

    added event listener

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -5,11 +5,19 @@ var DATA_CACHE = "budget-data-cache";
 //create an array to store the urls i want to save in cache
 
 var applicationUrls = [
-    "/",
-    "/db.js",
-    "/index.js",
-    "/styles.css",
-    "/manifest.json",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png"
-]
+  "/",
+  "/db.js",
+  "/index.js",
+  "/styles.css",
+  "/manifest.json",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE).then(function (cache) {
+      return cache.addAll(applicationUrls);
+    })
+  );
+});
