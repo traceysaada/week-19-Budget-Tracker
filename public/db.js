eb5b1e9355abf69076d4df01302a3c30d4a6ec8d
Tracey Saada <tracey.saada@outlook.com>
2021-03-19 18:16:02 +0000

    Formatted Manifest

--- public/db.js
+++ public/db.js
@@ -1,6 +1,63 @@
-// this file is all about setting up indexedDB 
+if("serviceWorker" in navigator){
+    window.addEventListener('load', function(){
+            navigator.serviceWorker.register('/server-worker', {scope: '/'}).then(function(reg){
+                console.log("Service Worker Registered")
+            })
+    })
+}
 
-//in this file we will have an event listener on window itself waiting for it to come online...
+const indexedDB = window.indexedDB;
+let db;
+// this file is all about setting up indexedDB
+const request = indexedDB.open("budget", 1);
 
-// when the application comes online - we need to check the database, get each entry in the db... 
-// then send them all to the /api/transaction/bulk end point 
+request.onupgradeneeded = function (event) {
+  //in this file we will have an event listener on window itself waiting for it to come online...
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+  db = event.target.result;
+  if (navigator.online) {
+    checkDatabase();
+  }
+};
+request.onerror = function (event) {
+  console.log("whoops!" + event.target.errorCode);
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(["pending"], "readwrite");
+
+  const store = transaction.objectStore("pending");
+
+  store.add(record);
+}
+function checkDatabase() {
+  const transaction = db.transaction(["pending"], "readwrite");
+  const store = transaction.objectStore("pending");
+
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json)
+        .then(() => {
+          const transaction = db.transaction(["pending"], "readwrite");
+          const store = transaction.objectStore("pending");
+          store.clear();
+        });
+    }
+  };
+}
+// when the application comes online - we need to check the database, get each entry in the db...
+window.addEventListener("online", checkDatabase);
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 //create variables that name the cache
 var CACHE = "budget-cache";
 var DATA_CACHE = "budget-data-cache";
@@ -20,14 +22,54 @@ self.addEventListener("install", function (event) {
       return cache.addAll(applicationUrls);
     })
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', function(event){
-  //implement functionality which will enable all requests coming in on /api routes  (think.... if statements)
-    //attempt to put into cache if its an api request
-    //if unsuccessful then it going to try to match with something in cache that already exsists
+self.addEventListener("activate", function (evt) {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
 
-  // if its not an api request then we will check the cache to see if we faciliate such a request 
-    // if the requested url isnt in cache then just return to hompage ('/')
-  
-})
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+  //implement functionality which will enable all requests coming in on /api routes  (think.... if statements)
+  //attempt to put into cache if its an api request
+  if (evt.request.url.includes("/api/")) {
+        evt.resondWith (
+          caches.open(DATA_CACHE_NAME).then(cache => {
+            return fetch(evt.request)
+            .then(response => {
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch(err => {
+              return caches.match(evt.request)
+            });
+          }).catch(err => console.log(err))
+        );
+        return;
+  }
+  //if unsuccessful then it going to try to match with something in cache that already exsists
+  // if its not an api request then we will check the cache to see if we faciliate such a request
+  // if the requested url isnt in cache then just return to hompage ('/')
+
+evt.resondWith(
+  caches.match(evt.request).then(function(response){
+    return response || fetch(evt.request);
+  })
+ );
+});
