8dc9773c3a25863f74cf2c84aa40aced1fc31196
Tracey Saada <tracey.saada@outlook.com>
2021-03-19 18:53:08 +0000

    Fixing bugs

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 //create variables that name the cache
-var CACHE = "budget-cache";
-var DATA_CACHE = "budget-data-cache";
+var CACHE = "my-budget-cache";
+var DATA_CACHE_NAME = "budget-data-cache";
 
 //create an array to store the urls i want to save in cache
 
@@ -9,49 +9,48 @@ var applicationUrls = [
   "/db.js",
   "/index.js",
   "/styles.css",
-  "/manifest.json",
+  "/manifest.webmanifest",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
 ];
 
 self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open(CACHE).then(function (cache) {
-      cache.addAll(applicationUrls);
+    caches.open(CACHE).then((cache) => {
+     return cache.addAll(applicationUrls);
     })
   );
- 
 });
 
 
 self.addEventListener("fetch", function (event) {
   //implement functionality which will enable all requests coming in on /api routes  (think.... if statements)
   //attempt to put into cache if its an api request
-  if (evt.request.url.includes("/api/")) {
-        evt.resondWith (
-          caches.open(DATA_CACHE_NAME).then(cache => {
-            return fetch(evt.request)
-            .then(response => {
+  if (event.request.url.includes("/api/")) {
+        event.respondWith(
+          caches.open(DATA_CACHE_NAME).then((cache) => {
+            return fetch(event.request)
+            .then((response) => {
               if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
+                cache.put(event.request.url, response.clone());
               }
 
               return response;
             })
-            .catch(err => {
-              return caches.match(evt.request)
+            .catch(() => {
+              return cache.match(event.request)
             });
-          }).catch(err => console.log(err))
-        );
+          }).catch((err) => console.log(err))
+         );
         return;
   }
-  //if unsuccessful then it going to try to match with something in cache that already exsists
-  // if its not an api request then we will check the cache to see if we faciliate such a request
-  // if the requested url isnt in cache then just return to hompage ('/')
 
-evt.resondWith(
-  caches.match(evt.request).then(function(response){
-    return response || fetch(evt.request);
+
+event.respondWith(
+  fetch(event.request).catch(function(){
+    return caches.match(event.request).then(function(response){
+      return response
+    })
   })
  );
 });
